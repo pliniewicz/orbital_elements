dcc5d7b86a80ca29a86c9de1b5f1750e81403f38
pliniewicz <jazingek@gmail.com>
2022-10-12 13:16:00 +0200

    added RA and DEC, hard-coding for now

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,18 @@ let data1 = time::Date{year: 2028, month: 1, decimal_day: time::decimal_day(&tim
 let data2 = time::Date{year: 2028, month: 2, decimal_day: time::decimal_day(&time::DayOfMonth{day: 14, hr: 2, min: 0, sec: 0.0, time_zone: 0.0}), cal_type: time::CalType::Gregorian};
 let data3 = time::Date{year: 2028, month: 3, decimal_day: time::decimal_day(&time::DayOfMonth{day: 25, hr: 3, min: 0, sec: 0.0, time_zone: 0.0}), cal_type: time::CalType::Gregorian};
 
+// wkładam współrzędne
+let alpha1 = angle::deg_frm_hms(11, 42, 21.2960).to_radians();
+let alpha2 = angle::deg_frm_hms(11, 55, 35.4617).to_radians();
+let alpha3 = angle::deg_frm_hms(11, 32, 10.1277).to_radians();
+let delta1 = angle::deg_frm_dms(-14, 29, 39.806).to_radians();
+let delta2 = angle::deg_frm_dms(-7, 36, 14.928).to_radians();
+let delta3 = angle::deg_frm_dms(7, 51, 20.425).to_radians();
+
 // obliczam różnice dni juliańskich
-let time_difference1 = time::julian_day(&data3)-time::julian_day(&data2);
-let time_difference2 = time::julian_day(&data3)-time::julian_day(&data1);
-let time_difference3 = time::julian_day(&data2)-time::julian_day(&data1);
+let time_difference1 = ( time::julian_day(&data3)-time::julian_day(&data2) ) / TIME_UNIT;
+let time_difference2 = ( time::julian_day(&data3)-time::julian_day(&data1) ) / TIME_UNIT;
+let time_difference3 = ( time::julian_day(&data2)-time::julian_day(&data1) ) / TIME_UNIT;
 
 // wyznaczam współrzędne geocentryczne słońca w 3 chwilach czasu
 let (point1, dist1) = sun::geocent_ecl_pos(time::julian_day(&data1));
@@ -31,7 +39,9 @@ let (X2,Y2,Z2) = sun::geocent_rect_coords(point2.lat, point2.long, dist2, eclipt
 let (point3, dist3) = sun::geocent_ecl_pos(time::julian_day(&data3));
 let (X3,Y3,Z3) = sun::geocent_rect_coords(point3.lat, point3.long, dist3, ecliptic::mn_oblq_laskar(time::julian_day(&data3)));
 
-// println!("{}", data1);
+println!("t3-t1: {}", time_difference1);
+println!("t3-t2: {}", time_difference2);
+println!("t2-t1: {}", time_difference3);
 println!("X1 :{} au\nY1: {} au\nZ1: {} au" ,X1, Y1, Z1);
 println!("X2 :{} au\nY2: {} au\nZ2: {} au" ,X2, Y2, Z2);
 println!("X3 :{} au\nY3: {} au\nZ3: {} au" ,X3, Y3, Z3);
